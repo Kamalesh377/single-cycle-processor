2116aa9cb911b23c011c7bdbace7b284a6d25c31
shrithaa <116354089+shrithaa@users.noreply.github.com>
2023-11-18 22:35:32 +0530

    alu.sv

--- alu.sv
+++ alu.sv
@@ -1,11 +1,16 @@
 module alu(
   input logic [31:0] operand1,
-  input logic [31:0] operand2,
+  input logic [31:0] operand2_reg,
+  input logic [31:0] operand2_imm,
   input string operation,
   input bit muxcontrol,
   output logic [31:0] alu_output,
   output bit zero_flag
 );
+logic [31:0] operand2;
+assign operand2=muxcontrol ? (operand2_reg):(operand2_imm);
+
+
 
   always_comb begin
     case(operation)
@@ -13,8 +18,8 @@ module alu(
       "sub": alu_output = operand1 - operand2;
       "and": alu_output = operand1 & operand2;
       "or": alu_output = operand1 | operand2;
-      "lw": alu_output = operand1 + operand2;
-      "sw": alu_output = operand1 + operand2;
+      "lw": alu_output = operand1 + operand2_imm;
+      "sw": alu_output = operand1 + operand2_imm;
       "beq": alu_output = operand1 - operand2;
       default: alu_output = 32'b0; 
     endcase
@@ -28,3 +33,4 @@ module alu(
   end
 
 endmodule
+
