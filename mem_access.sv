136c39f2ca4d306f142ae59c93d8f58822ea2f34
shrithaa <116354089+shrithaa@users.noreply.github.com>
2023-11-24 22:26:49 +0530

    i have written code for only 5 sets of reg(it says not enough i/o ports but getting schematic

--- mem_access.sv
+++ mem_access.sv
@@ -6,12 +6,17 @@ module memory_access(
     output logic [31:0] data_out
 );
 
-    
-    logic [31:0] memory_array [0:5]; 
+    logic [31:0] memory_array [0:3]; 
+
+    // Initialize memory_array with ascending values
+    initial begin
+        for (int i = 0; i < 6; i = i + 1) begin
+            memory_array[i] = i;
+        end
+    end
 
     always_comb begin
         if (mem_read) begin
-            
             case (address)
                 32'h00000000: data_out = memory_array[0];
                 32'h00000004: data_out = memory_array[1];
@@ -19,22 +24,18 @@ module memory_access(
                 32'h0000000C: data_out = memory_array[3];
                 32'h00000010: data_out = memory_array[4];
                 32'h00000014: data_out = memory_array[5];
-               
                 default: data_out = 32'b0000_0000; 
             endcase
         end else if (mem_write) begin
-            
             case (address)
                 32'h00000000: memory_array[0] = write_data;
-                32'h00000004: memory_array[1] = write_data;
+               32'h00000004: memory_array[1] = write_data;
                 32'h00000008: memory_array[2] = write_data;
                 32'h0000000C: memory_array[3] = write_data;
                 32'h00000010: memory_array[4] = write_data;
                 32'h00000014: memory_array[5] = write_data;
-                
             endcase
         end else begin
-            
             data_out = 32'b0000_0000; 
         end
     end
