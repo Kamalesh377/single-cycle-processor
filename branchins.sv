6b3702472d051b6221a6f66208bcf663dbf38d6e
shrithaa <116354089+shrithaa@users.noreply.github.com>
2023-11-25 09:26:19 +0530

    Update branchins.sv added and gate

--- branchins.sv
+++ branchins.sv
@@ -1,10 +1,13 @@
 module branchins(
     input logic [31:0] immgen,
     input logic [31:0] pc,
-    output logic [31:0] pc_branch
+    output logic [31:0] pc_branch,
+    input logic branch,
+    input logic zero_flag
 );
 
 logic [31:0] imm;
+assign muxcontol= branch&zero_flag;
 
 always_comb begin
     imm = immgen << 1;
@@ -14,6 +17,8 @@ always_comb begin
     end else begin
         pc_branch = pc - imm;
     end
+    
+    
 end
 
 endmodule
